e684eea62d7813dd5299e27afc0e1d4888f4c24b
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-04-19 19:06:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let list = document.querySelectorAll('li');
+
+        let n = list.length;
+        let ans;
+        for(let i=0;i<n;i++){
+            if(list[i].id=='level')
+            ans = i;
+        }
+
+ans++;
+        alert('The level of the element is: '+ ans);
